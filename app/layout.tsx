e32bbaa48e6024c355bcbb24012799960b2f4ad2
Alpha-4 <aarondadhwal@gmail.com>
2024-04-09 23:08:08 +0530

    initial app setup And adding clerk for auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
+import {ClerkProvider} from '@clerk/nextjs'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
