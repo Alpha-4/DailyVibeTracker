a220a271fd3f37b018c5e33ada53eabb455972de
Alpha-4 <aarondadhwal@gmail.com>
2024-04-15 22:33:52 +0530

    adding correct redirecting from the main page

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import {prisma} from "@/utils/db"
+import {currentUser} from "@clerk/nextjs"
+import {User} from "@clerk/nextjs/dist/types/server";
+import {redirect} from "next/navigation";
 
-const page = () => {
+
+const createNewUser = async () => {
+
+    const user: User = (await currentUser())!;
+    const dbUser = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id
+        }
+    })
+
+    if (!dbUser) {
+        const newUser = await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user?.emailAddresses[0]?.emailAddress
+            }
+        })
+    }
+    redirect("/journal")
+}
+
+const page = async () => {
+    await createNewUser();
     return (
         <div>Hi New user!!</div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import {auth} from "@clerk/nextjs";
 import Link from "next/link";
 
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth();
+  let href = userId ? `/journal` : `/new-user`;
   return (
     <div className="w-screen h-screen bg-blue-950/90 flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto space-y-2">
@@ -10,7 +13,7 @@ export default function Home() {
         </h1>
         <p className="text-2xl text-white/65">Best app to keep track of your daily vibes. </p>
         <div>
-          <Link href="/journal">
+          <Link href={href}>
             <button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Get Started</button>
           </Link>
         </div>
